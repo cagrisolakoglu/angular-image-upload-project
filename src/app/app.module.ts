62dbef9623ab5ca1dfc423678c1eea4ea3001577
çağrı solakoğlu <cagri.solakoglu@ikcu.edu.tr>
2022-08-18 10:12:48 +0300

    package update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,10 @@ import { ImageCropperModule } from 'ngx-image-cropper';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ImageModuleModule } from './image-module/image-module.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import { ButtonModule } from 'primeng/button';
+import { RippleModule } from 'primeng/ripple';
 
 @NgModule({
   declarations: [
@@ -12,9 +16,12 @@ import { ImageModuleModule } from './image-module/image-module.module';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ImageModuleModule,
-    ImageCropperModule
+    ImageCropperModule,
+    ButtonModule,
+    RippleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
